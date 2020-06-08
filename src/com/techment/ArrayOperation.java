8ac7210f39281aab3f7b7374db1bb1eb32b74342
Ajit Kumar <38699939+deadlyjack@users.noreply.github.com>
2020-06-08 11:33:05 +0530

    Create ArrayOperation.java
    Aditi and Muskan complete those todo list in the file

---
+++ src/com/techment/ArrayOperation.java
@@ -0,0 +1,15 @@
+/*
+* First commit from Ajit Kumar
+*/
+package com.techement;
+
+public class ArrayOperation{
+
+   public static void main(String args[]){
+      int array[] = {3,4,5,6};
+      
+      //TODO: Add feature to add array
+      //TODO: Ard feature to print array
+   }
+
+}
