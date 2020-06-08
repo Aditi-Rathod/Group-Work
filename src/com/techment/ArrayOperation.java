b4021566f1be34caa0d2b6b2afcc6c067594a324
Ajit Kumar <38699939+deadlyjack@users.noreply.github.com>
2020-06-08 12:32:04 +0530

    Update ArrayOperation.java

--- src/com/techment/ArrayOperation.java
+++ src/com/techment/ArrayOperation.java
@@ -1,15 +1,24 @@
+
 /*
 * First commit from Ajit Kumar
+* Second commit from Aditi Rathod
+*Thrid commit from Muskan Goyal
 */
 package com.techement;
 
 public class ArrayOperation{
 
    public static void main(String args[]){
-      int array[] = {3,4,5,6};
+      int array[] = {3,4,5,6},i,sum=0;
       
       //TODO: Add feature to add array
-      //TODO: Ard feature to print array
+      for(i=0;i<=4;i++) //comment by AJit Kumar
+      {
+         sum=sum+array[i];
+      }
+      //TODO: Add feature to print  sum
+       system.out.println("The Sum of array elements is:\n"+sum);
    }
+   
 
 }
